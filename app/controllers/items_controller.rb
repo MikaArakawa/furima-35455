122c111e616cf2e767476669ddeb05369fe0a2ef
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-25 21:59:00 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,10 @@ class ItemsController < ApplicationController
   def index
   end
       
+  private
+
+  def message_params
+    params.require(:item).permit(:title, :description, :price, :status_id, :category_id,:shipping_charge_id, :prefecture_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
 
+  with_options presence: true do
+    validates :title                   
+    validates :description            
+    validates :price,                   format: {with: /\A[0-9]+\z/}, numericality: { in: 300..9999999 }
+    validates :status_id             
+    validates :category_id           
+    validates :shipping_charge_id      
+    validates :prefecture_id         
+    validates :shipping_date_id        
+  end
+
   has_one :purchased_item, dependent: :destroy
   belongs_to :user, dependent: :destroy
+  has_one_attached :image
+
 end

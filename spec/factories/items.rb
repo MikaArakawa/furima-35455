1a9dd6fc87f96f8952b179d20f4f08f67734d95d
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-26 13:35:49 +0900

    モデル単体テストのexamples記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :item do
+    title                    { 'title' }
+    description              { 'description' }
+    price                    { '300' }
+    status_id                { '2' }
+    category_id              { '2' }
+    shipping_charge_id       { '2' }
+    prefecture_id            { '2' }
+    shipping_date_id         { '2' }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('app/aassets/images/flag.png')
+    end
+
+    context '内容に問題がない場合' do
+      it '必要な情報を適切に入力すると、商品情報がデータベースに保存されること' do
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it '商品画像を1枚つけることが必須であること' do
+      end
+
+      it '商品名が必須であること' do
+      end
+
+      it '商品の説明が必須であること' do
+      end
+
+      it 'カテゴリーの情報が必須であること' do
+      end
+
+      it '商品の状態についての情報が必須であること' do
+      end
+
+      it '配送料の負担についての情報が必須であること' do
+      end
+
+      it '発送元の地域についての情報が必須であること' do
+      end
+
+      it '発送までの日数についての情報が必須であること' do
+      end
+
+      it '販売価格についての情報が必須であること' do
+      end
+
+      it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
+      end
+
+      it '販売価格は半角数字のみ保存可能であること' do
+      end
+    end
+  end
 end

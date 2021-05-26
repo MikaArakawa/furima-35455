e1483796aacd49edc29e7e9f07353e76d2fb438c
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-26 12:02:30 +0900

    出品した商品の情報をテーブルに保存できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ class ItemsController < ApplicationController
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,24 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title,                   length: { maximum: 40 }
     validates :description,             length: { maximum: 1000 } 
-    validates :price,                   format: {with: /\A[0-9]+\z/}, numericality: { in: 300..9999999 }
-    validates :status_id             
+    validates :price,                   format: {with: /\A[0-9]+\z/}
+    validates :status_id                         
     validates :category_id           
     validates :shipping_charge_id      
     validates :prefecture_id         
     validates :shipping_date_id        
     validates :image
   end
+
+  validates_inclusion_of :price, in:300..9999999 
+  validates :prefecture_id, :status_id, :category_id,:shipping_date_id, :shipping_charge_id, numericality: { other_than: 1 } 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :category
+  belongs_to :shipping_date
+  belongs_to :shipping_charge
 
   has_one :purchased_item, dependent: :destroy
   belongs_to :user, dependent: :destroy

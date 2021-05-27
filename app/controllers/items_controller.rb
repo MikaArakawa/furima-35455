bac7b83a3eb1bf6ed7f6ee7855020be60bf526eb
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-27 14:16:55 +0900

    Viewファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_item, except: [:index, :new, :create]
-  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,18 +20,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :description, :price, :status_id, :category_id, :shipping_charge_id, :prefecture_id,
-                                :shipping_date_id, :image).merge(user_id: current_user.id)
+                                 :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 
-#   def set_item
-#     @item = Item.find(params[:id])
-#   end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
-#   def contributor_confirmation
-#     redirect_to root_path unless current_user == @item.user
-#   end
-# end
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+end

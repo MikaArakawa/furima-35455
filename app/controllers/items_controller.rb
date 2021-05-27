2ca0b7cb23f4e589d1d376c5da4277373375331c
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-27 14:34:30 +0900

    不要な記述の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, except: [:index, :new, :create]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  # before_action :set_item, except: [:index, :new, :create]
+  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -30,11 +30,11 @@ class ItemsController < ApplicationController
                                  :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user
-  end
+  # def contributor_confirmation
+  #   redirect_to root_path unless current_user == @item.user
+  # end
 end

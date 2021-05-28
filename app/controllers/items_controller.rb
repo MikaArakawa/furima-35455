184544469bb67e7dbb532347da069b3731626096
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-28 09:56:37 +0900

    updateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
-  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -23,6 +23,17 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def edit
+  end 
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
@@ -34,7 +45,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def contributor_confirmation
-  #   redirect_to root_path unless current_user == @item.user
-  # end
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
 end

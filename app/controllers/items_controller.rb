1c426922845d0502c0493ca02b58fc734d5480a3
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-28 10:31:47 +0900

    ControllerにDestroyアクションを追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
-  before_action :contributor_confirmation, only: [:edit, :update] #:destroy
+  before_action :contributor_confirmation, only: [:edit, :update,:destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -34,6 +34,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path(@item)
+    end
+  end
+  
   private
 
   def item_params

88c15678c92e2b42d9ecef86f0ee5c993c0a7eeb
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-28 13:52:55 +0900

    クレジットカード決済機能導入の準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_date
   belongs_to :shipping_charge
 
-  # has_one :purchased_item, dependent: :destroy
+  # has_one :order, dependent: :destroy
   belongs_to :user 
   has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_083825) do
+ActiveRecord::Schema.define(version: 2021_05_28_042252) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2021_05_25_083825) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

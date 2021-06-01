793857b15140f657c2fac04e06efd0c95624800f
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-06-01 17:11:03 +0900

    手順1 JavaScriptでtokenを生成してサーバーサイドへ送信する

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,26 +1,35 @@
-// const pay = () => {
-//   Payjp.setPublicKey("pk_test_b6ebae680aa425b8867dfd77"); 
-//   const form = document.getElementById("charge-form");
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
+const pay = () => {
+  Payjp.setPublicKey("pk_test_b6ebae680aa425b8867dfd77"); 
+  const form = document.getElementById('charge-form');
+  form.addEventListener("submit", (e)  =>{
+    e.preventDefault();
 
-//     const formResult = document.getElementById("charge-form");
-//     const formData = new FormData(formResult);
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
 
-//     const card = {
-//       number: formData.get("order[number]"),
-//       cvc: formData.get("order[cvc]"),
-//       exp_month: formData.get("order[exp_month]"),
-//       exp_year: `20${formData.get("order[exp_year]")}`,
-//     };
+    const card = {
+      number: formData.get("order_address[number]"),
+      exp_month: formData.get("order_address[exp_month]"),
+      exp_year: `20${formData.get("order_address[exp_year]")}`,
+      cvc: formData.get("order_address[cvc]"),
+    };
 
-//     Payjp.createToken(card, (status, response) => {
-//       if (status == 200) {
-//         const token = response.id;
-//         console.log(token)
-//       }
-//     });
-//   });
-// };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
 
-// window.addEventListener("load", pay);
+      document.getElementById("order_address_number").removeAttribute("name");
+      document.getElementById("order_address_cvc").removeAttribute("name");
+      document.getElementById("order_address_exp_month").removeAttribute("name");
+      document.getElementById("order_address_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
